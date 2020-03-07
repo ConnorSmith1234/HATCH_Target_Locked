d06b4a046164b020581d471ce24448a849143f23
Tyler Smith <35615709+tms0050@users.noreply.github.com>
2020-03-07 13:12:24 -0600

    .Net Core Base Commit
    basic .net core project created through visual studio

---
+++ TargetLocked/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TargetLocked.Models;
+
+namespace TargetLocked.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search([FromBody]string searchQuery)
+        {
+            // call python script with search query. Get model back, return model as Json(modelObject);
+            return Ok(200);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
