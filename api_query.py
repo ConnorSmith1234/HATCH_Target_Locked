78c2dde7806d8c71c3b5fe17d6b78d489779f4b2
Muthukumaran R <mr0051@uah.edu>
2020-03-07 19:13:56 -0600

    adds HGMD api query

--- api_query.py
+++ api_query.py
@@ -1,42 +1,26 @@
-import scholarly
+import requests
 
-def PubMed(search_term):
-    return 'what they return'
-
-def google_scholar(search_term, num_documents=10):
-    all_query_results = scholarly.search_pubs_query(search_term)
-
-    query_results = []
-    for i in range(num_documents):
-        print(i)
-        try:
-            pub = next(all_query_results)
-        except StopIteration:
-            break
-
-        metadata = {}
-        metadata['abstract'] = pub.bib.get('abstract', None)
-        metadata['title'] = pub.bib.get('title', None)
-        metadata['author'] = pub.bib.get('author', None)
-        metadata['year'] = pub.bib.get('year', None)
-        metadata['journal'] = pub.bib.get('journal', None)
-        metadata['publisher'] = pub.bib.get('publisher', None)
-        metadata['ID'] = pub.bib.get('ID', None)
-        metadata['url'] = pub.bib.get('url', None)
-        metadata['eprint'] = pub.bib.get('eprint', None)
-        metadata['num_citations'] = pub.citedby
-        metadata['document_type'] = pub.bib.get('ENTRYTYPE', None)
-
-        query_results.append(metadata)
-
-    print(f'{len(query_results)} returned')
-
-    return query_results
+from bs4 import BeautifulSoup
 
 
-def BioArchive():
-    pass
+USER_COOKIE = {'PHPSESSID': '6lm3i3jevbhqhbu1o3fsnm3u64',
+               'NSC_iptujoh.dg.bd.vl-iuuq-wt': 'ffffffff09f7fa6f45525d5f4f58455e445a4a423660',
+               'path': '/',
+               'domain': '.www.hgmd.cf.ac.uk',
+               'Expires': 'Tue, 19 Jan 2038 03:14:07 GMT'
+               }
+GENE_URL = f'http://www.hgmd.cf.ac.uk/ac/gene.php?gene={gene_name}'
 
-def MedArchive():
-    pass
 
+def get_diseases_for_gene(gene_name):
+    """get disease for gene name from HGMD database
+
+    Args:
+        gene_name (TYPE): name of gene
+
+    Returns:
+        TYPE: list of diseases
+    """
+    resp = requests.get(GENE_URL, cookies=USER_COOKIE)
+    parsed_resp = resp.find_all('table')[2].find_all('td')[0::3]
+    return [BeautifulSoup.get_text(disease) for disease in parsed_resp]
