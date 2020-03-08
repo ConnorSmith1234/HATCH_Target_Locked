0fcbc07162195db6fd3611c37cbea355eb333a50
Muthukumaran R <mr0051@uah.edu>
2020-03-07 19:35:07 -0600

    bug fix HGMD api query

--- api_query.py
+++ api_query.py
@@ -9,7 +9,6 @@ USER_COOKIE = {'PHPSESSID': '6lm3i3jevbhqhbu1o3fsnm3u64',
                'domain': '.www.hgmd.cf.ac.uk',
                'Expires': 'Tue, 19 Jan 2038 03:14:07 GMT'
                }
-GENE_URL = f'http://www.hgmd.cf.ac.uk/ac/gene.php?gene={gene_name}'
 
 
 def get_diseases_for_gene(gene_name):
@@ -21,6 +20,12 @@ def get_diseases_for_gene(gene_name):
     Returns:
         TYPE: list of diseases
     """
+    GENE_URL = f'http://www.hgmd.cf.ac.uk/ac/gene.php?gene={gene_name}'
     resp = requests.get(GENE_URL, cookies=USER_COOKIE)
-    parsed_resp = resp.find_all('table')[2].find_all('td')[0::3]
-    return [BeautifulSoup.get_text(disease) for disease in parsed_resp]
+    parsed_resp = BeautifulSoup(resp.text, 'lxml')
+
+    try:
+        parsed_resp = parsed_resp.find_all('table')[2].find_all('td')[0::3]
+        return [BeautifulSoup.get_text(disease) for disease in parsed_resp]
+    except IndexError:
+        return []
